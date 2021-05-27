dfadc64e31212bf3d520170af721df35f5f82960
Tim Dill <brodt7258@gmail.com>
2021-05-26 22:14:41 -0400

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 let data = require('./MOCK_DATA.json');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/mentees', (req, res) => {
   if (req.query.q) {
